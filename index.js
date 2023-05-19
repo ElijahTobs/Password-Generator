05ad08b74f56bf76d7f5914d6835d05dfdaccee3
Comfort Ige <eligenius@gmail.com>
2023-05-19 20:21:53 +0100

    Add random num and char variables

---
+++ index.js
@@ -0,0 +1,11 @@
+let getPwd = document.getElementById("pwd-gen")
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let rand1 = Math.floor(Math.random() * characters.length)
+let rand2 = Math.floor(Math.random() * characters.length)
+
+getPwd.addEventListener("click", function(){
+
+})
+
